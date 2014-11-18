45ab840094b62326e17d57c39e243723b4515e35
jai11 <jai.jagani@yahoo.in>
2014-11-18 21:15:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$3';"
 Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
 Q4="FLUSH PRIVILEGES;"
 Q5="use nodejs;"
-Q6="source /root/nodecrud/customer.sql;"
+Q6="source customer.sql;"
 SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
  
 if [ $# -ne $EXPECTED_ARGS ]
@@ -18,4 +18,4 @@ then
   exit $E_BADARGS
 fi
  
-$MYSQL -u root -p -e "$SQL"
+$MYSQL -u root -e "$SQL"
