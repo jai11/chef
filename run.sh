6d7c83a2c0c3fa2f2d5051c3fa0779a55d4fffdc
jai11 <jai.jagani@yahoo.in>
2014-11-18 14:44:09 +0000

    first commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash
+ 
+EXPECTED_ARGS=3
+E_BADARGS=65
+MYSQL=`which mysql`
+ 
+Q1="CREATE DATABASE IF NOT EXISTS $1;"
+Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$3';"
+Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
+Q4="FLUSH PRIVILEGES;"
+Q5="use nodejs;"
+Q6="source /root/nodecrud/customer.sql;"
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
+ 
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 dbname dbuser dbpass"
+  exit $E_BADARGS
+fi
+ 
+$MYSQL -u root -p -e "$SQL"
